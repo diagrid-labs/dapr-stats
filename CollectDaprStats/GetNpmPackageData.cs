6afb403c8a4ae4d4b853021dc812a169708a40cb
Marc Duiker <marcduiker@users.noreply.github.com>
2023-12-13 21:56:09 +0100

    Add GetNpmPackageData
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

---
+++ CollectDaprStats/GetNpmPackageData.cs
@@ -0,0 +1,79 @@
+using Azure.Data.Tables;
+using Azure;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace CollectDaprStats
+{
+    public class GetNpmPackageData
+    {
+        private readonly HttpClient _httpClient;
+
+        public GetNpmPackageData(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        [Function(nameof(GetNpmPackageData))]
+        [TableOutput("NpmDapr", Connection = "AzureWebJobsStorage")]
+        public async Task<IEnumerable<NpmPackageVersionData>> Run(
+            [ActivityTrigger] string packageName, FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger(nameof(GetNpmPackageData));
+            var npmPackageVersionDataList = new List<NpmPackageVersionData>();
+
+            _httpClient.BaseAddress = new Uri("https://api.npmjs.org/");
+            packageName = WebUtility.UrlEncode(packageName);
+            var response = await _httpClient.GetAsync($"versions/{packageName}/last-week");
+            if (response.IsSuccessStatusCode)
+            {
+                var npmPackageVersionResponse = await response.Content.ReadFromJsonAsync<NpmPackageVersionResponse>();
+                foreach (var versionPair in npmPackageVersionResponse.Downloads)
+                {
+                    var npmPackageVersionData = new NpmPackageVersionData
+                    {
+                        DayOfYear = DateTime.UtcNow.DayOfYear,
+                        PackageName = npmPackageVersionResponse.Package,
+                        VersionString = versionPair.Key,
+                        Downloads = versionPair.Value,
+                        PartitionKey = DateTime.UtcNow.DayOfYear.ToString(),
+                        RowKey = $"{npmPackageVersionResponse.Package}-{versionPair.Key}"
+                    };
+                    npmPackageVersionDataList.Add(npmPackageVersionData);
+                    logger.LogInformation(npmPackageVersionData.ToString());
+                }
+            }
+            else
+            {
+                logger.LogError($"Failed to get data for {packageName}");
+            }
+
+            return npmPackageVersionDataList;
+        }
+    }
+
+    public class NpmPackageVersionResponse
+    {
+        public string Package { get; set; }
+        public Dictionary<string, int> Downloads { get; set; }
+    }
+
+    public class NpmPackageVersionData : ITableEntity
+    {
+        public int DayOfYear { get; set; }
+        public string PackageName { get; set; }
+        public string? VersionString { get; set; }
+        public long? Downloads { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        public override string ToString()
+        {
+            return $"{PackageName} {VersionString} {Downloads}";
+        }
+    }
+}
