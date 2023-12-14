1b0b2d4360e14fdcaad31cac345363c6fc78fbcc
Marc Duiker <marcduiker@users.noreply.github.com>
2023-12-14 09:37:27 +0100

    Move starter to own function
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- CollectDaprStats/CollectorOrchestrator.cs
+++ CollectDaprStats/CollectorOrchestrator.cs
@@ -1,8 +1,6 @@
 using CollectDaprStats;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.DurableTask;
-using Microsoft.DurableTask.Client;
 using Microsoft.Extensions.Logging;
 
 namespace Dapr
@@ -23,25 +21,6 @@ namespace Dapr
                 "@dapr/dapr");
             await context.CallActivityAsync<DiscordData>(
                 nameof(GetDiscordData));
-        }
-
-        [Function("DurableFunctionsOrchestrationCSharp1_HttpStart")]
-        public static async Task<HttpResponseData> HttpStart(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
-            [DurableClient] DurableTaskClient client,
-            FunctionContext executionContext)
-        {
-            ILogger logger = executionContext.GetLogger("DurableFunctionsOrchestrationCSharp1_HttpStart");
-
-            // Function input comes from the request content.
-            string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(
-                nameof(CollectorOrchestrator));
-
-            logger.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
-
-            // Returns an HTTP 202 response with an instance management payload.
-            // See https://learn.microsoft.com/azure/azure-functions/durable/durable-functions-http-api#start-orchestration
-            return client.CreateCheckStatusResponse(req, instanceId);
         }
     }
 }
