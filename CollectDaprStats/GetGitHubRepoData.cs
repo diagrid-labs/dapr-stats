c348e2099bbd8ebd4d32e1f5571e5faf5698e749
Marc Duiker <marcduiker@users.noreply.github.com>
2024-02-15 15:18:45 +0100

    Add logging to GitHub activity
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- CollectDaprStats/GetGitHubRepoData.cs
+++ CollectDaprStats/GetGitHubRepoData.cs
@@ -7,20 +7,23 @@ namespace DaprStats
     {
         private readonly PostgresOutput _output;
         private readonly IGitHubClient _gitHubClient;
+        private readonly ILogger _logger;
 
         public GetGitHubRepoData(
             IGitHubClient gitHubClient,
-            PostgresOutput output)
+            PostgresOutput output,
+            ILoggerFactory loggerFactory)
         {
             _gitHubClient = gitHubClient;
             _output = output;
+            _logger = loggerFactory.CreateLogger<GetGitHubRepoData>();
         }
 
         public override async Task<bool> RunAsync(
             WorkflowActivityContext context,
             GitHubDataInput input)
         {
-            const int CollectionPeriodInDays = 7;
+            const int CollectionPeriodInDays = 14;
             var apiOptions = new ApiOptions { PageSize = 250, StartPage = 1 };
             var repository = await _gitHubClient.Repository.Get(input.Organization, input.Repository);
 
@@ -49,92 +52,125 @@ namespace DaprStats
                 CollectedOverNumberOfDays = CollectionPeriodInDays
             };
 
-            string tableName = $"github_dapr";
-            var sqlText = $"insert into {tableName} (repo_name, collection_date, fork_count_total, star_count_total, commit_count, commit_users, issue_count, issue_users, comment_count, comment_users, pullrequest_count, pullrequest_users, distinct_user_count, collected_over_number_of_days) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)";
-            var sqlParameters = new object[] { githubData.Repository, githubData.CollectionDate, githubData.ForksTotalCount, githubData.StarsTotalCount, githubData.CommitCount, githubData.CommitUsers, githubData.IssueCount, githubData.IssueUsers, githubData.CommentCount, githubData.CommentUsers, githubData.PullRequestCount, githubData.PullRequestUsers, githubData.DistinctUserCount, githubData.CollectedOverNumberOfDays };
-            await _output.InsertAsync(sqlText, sqlParameters);
+            // string tableName = $"github_dapr";
+            // var sqlText = $"insert into {tableName} (repo_name, collection_date, fork_count_total, star_count_total, commit_count, commit_users, issue_count, issue_users, comment_count, comment_users, pullrequest_count, pullrequest_users, distinct_user_count, collected_over_number_of_days) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)";
+            // var sqlParameters = new object[] { githubData.Repository, githubData.CollectionDate, githubData.ForksTotalCount, githubData.StarsTotalCount, githubData.CommitCount, githubData.CommitUsers, githubData.IssueCount, githubData.IssueUsers, githubData.CommentCount, githubData.CommentUsers, githubData.PullRequestCount, githubData.PullRequestUsers, githubData.DistinctUserCount, githubData.CollectedOverNumberOfDays };
+            // await _output.InsertAsync(sqlText, sqlParameters);
             return true;
         }
 
         private async Task<CommitOutput> GetCommitData(GitHubDataInput input, Repository repository, int collectionPeriodInDays, ApiOptions apiOptions)
         {
-            var commitRequest = new CommitRequest { Since = input.CollectionDate.AddDays(-collectionPeriodInDays) };
-            var commitList = await _gitHubClient.Repository.Commit.GetAll(repository.Id, commitRequest, apiOptions);
-            var commitCountOverPeriod = commitList.Count;
-            var commitShas = string.Join(',', commitList.Select(commit => commit.Sha));
-            var distinctCommitUserNames = commitList.Select(commit => commit.Author.Login).Distinct();
-            var joinedDistinctCommitUserNames = string.Join(',', distinctCommitUserNames);
-            Console.WriteLine($"Repo: {repository.Name}, Commits: {commitCountOverPeriod}, Shas: {commitShas}, Users: {joinedDistinctCommitUserNames}");
+            try
+            {
+                var commitRequest = new CommitRequest { Since = input.CollectionDate.AddDays(-collectionPeriodInDays) };
+                var commitList = await _gitHubClient.Repository.Commit.GetAll(repository.Id, commitRequest, apiOptions);
+                var commitCountOverPeriod = commitList.Count;
+                var commitShas = string.Join(',', commitList.Select(commit => commit.Sha));
+                // List the distinct user names for the commits
+                var distinctCommitUserNames = commitList.Select(commit => commit.Author?.Login).Distinct();
+                var joinedDistinctCommitUserNames = string.Join(',', distinctCommitUserNames);
+                Console.WriteLine($"Repo: {repository.Name}, Commits: {commitCountOverPeriod}, Shas: {commitShas}, Users: {joinedDistinctCommitUserNames}");
 
-            return new CommitOutput(
-                commitCountOverPeriod,
-                distinctCommitUserNames,
-                joinedDistinctCommitUserNames);
+                return new CommitOutput(
+                    commitCountOverPeriod,
+                    distinctCommitUserNames,
+                    joinedDistinctCommitUserNames);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error getting commit data for {Repository}: {Exception}", repository.Name, ex);
+                throw;
+            }
         }
 
         private async Task<IssueOutput> GetIssueData(GitHubDataInput input, Repository repository, int collectionPeriodInDays, ApiOptions apiOptions)
         {
-            var issueRequest = new RepositoryIssueRequest
+            try
             {
-                Since = input.CollectionDate.AddDays(-collectionPeriodInDays),
-                Filter = IssueFilter.All,
-                SortProperty = IssueSort.Updated,
-                SortDirection = SortDirection.Descending
-            };
-            var issueList = await _gitHubClient.Issue.GetAllForRepository(repository.Id, issueRequest, apiOptions);
-            
-            var issueNumbers = string.Join(',', issueList.Select(issue => issue.Number));
-            var distinctIssueUserNames = issueList.Select(issue => issue.User.Login).Distinct();
-            var joinedDistinctIssueUserNames = string.Join(',', distinctIssueUserNames);
-            Console.WriteLine($"Repo: {repository.Name}, Issues: {issueList.Count}, Numbers: {issueNumbers}, Users: {joinedDistinctIssueUserNames}");
+                var issueRequest = new RepositoryIssueRequest
+                {
+                    Since = input.CollectionDate.AddDays(-collectionPeriodInDays),
+                    Filter = IssueFilter.All,
+                    SortProperty = IssueSort.Updated,
+                    SortDirection = SortDirection.Descending
+                };
+                var issueList = await _gitHubClient.Issue.GetAllForRepository(repository.Id, issueRequest, apiOptions);
 
-            return new IssueOutput
-            (
-                issueList.Count,
-                distinctIssueUserNames,
-                joinedDistinctIssueUserNames);
+                var issueNumbers = string.Join(',', issueList.Select(issue => issue.Number));
+                var distinctIssueUserNames = issueList.Select(issue => issue.User.Login).Distinct();
+                var joinedDistinctIssueUserNames = string.Join(',', distinctIssueUserNames);
+                Console.WriteLine($"Repo: {repository.Name}, Issues: {issueList.Count}, Numbers: {issueNumbers}, Users: {joinedDistinctIssueUserNames}");
+
+                return new IssueOutput
+                (
+                    issueList.Count,
+                    distinctIssueUserNames,
+                    joinedDistinctIssueUserNames);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error getting issue data for {Repository}: {Exception}", repository.Name, ex);
+                throw;
+            }
         }
 
         private async Task<CommentOutput> GetCommentData(GitHubDataInput input, Repository repository, int collectionPeriodInDays, ApiOptions apiOptions)
         {
-            var commentRequest = new IssueCommentRequest
+            try
             {
-                Since = input.CollectionDate.AddDays(-collectionPeriodInDays),
-                Sort = IssueCommentSort.Created,
-                Direction = SortDirection.Descending
-            };
-            var commentList = await _gitHubClient.Issue.Comment.GetAllForRepository(repository.Id, commentRequest, apiOptions);
-            var distinctCommentUserNames = commentList.Select(comment => comment.User.Login).Distinct();
-            var joinedDistinctCommentUserNames = string.Join(',', distinctCommentUserNames);
-            Console.WriteLine($"Repo: {repository.Name}, Comments: {commentList.Count}, Users: {joinedDistinctCommentUserNames}");
+                var commentRequest = new IssueCommentRequest
+                {
+                    Since = input.CollectionDate.AddDays(-collectionPeriodInDays),
+                    Sort = IssueCommentSort.Created,
+                    Direction = SortDirection.Descending
+                };
+                var commentList = await _gitHubClient.Issue.Comment.GetAllForRepository(repository.Id, commentRequest, apiOptions);
+                var distinctCommentUserNames = commentList.Select(comment => comment.User.Login).Distinct();
+                var joinedDistinctCommentUserNames = string.Join(',', distinctCommentUserNames);
+                Console.WriteLine($"Repo: {repository.Name}, Comments: {commentList.Count}, Users: {joinedDistinctCommentUserNames}");
 
-            return new CommentOutput
-            (
-                commentList.Count,
-                distinctCommentUserNames,
-                joinedDistinctCommentUserNames);
+                return new CommentOutput
+                (
+                    commentList.Count,
+                    distinctCommentUserNames,
+                    joinedDistinctCommentUserNames);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error getting comment data for {Repository}: {Exception}", repository.Name, ex);
+                throw;
+            }
         }
 
         private async Task<PullRequestOutput> GetPullRequestData(GitHubDataInput input, Repository repository, int collectionPeriodInDays, ApiOptions apiOptions)
         {
-            var prRequest = new PullRequestRequest { SortProperty = PullRequestSort.Created, SortDirection = SortDirection.Descending};
-            var prList = await _gitHubClient.Repository.PullRequest.GetAllForRepository(repository.Id, prRequest, apiOptions);
-            var prFilter =  new Func<PullRequest, bool>( pr =>
-                pr.CreatedAt > input.CollectionDate.AddDays(-collectionPeriodInDays) ||
-                pr.UpdatedAt > input.CollectionDate.AddDays(-collectionPeriodInDays) ||
-                pr.MergedAt > input.CollectionDate.AddDays(-collectionPeriodInDays) ||
-                pr.ClosedAt > input.CollectionDate.AddDays(-collectionPeriodInDays));
-            var filteredPrList = prList.Where(prFilter);
-            var distinctFilteredPrUserNames = filteredPrList.Select(pr => pr.User.Login).Distinct();
-            var joinedDistinctFilteredPrUserNames = string.Join(',', distinctFilteredPrUserNames);
-            var filteredPrCountOverPeriod = filteredPrList.Count();
-            var filteredPrNumbers = string.Join(',', filteredPrList.Select(pr => pr.Number));
-            Console.WriteLine($"Repo: {repository.Name}, PRs: {filteredPrCountOverPeriod}, Numbers: {filteredPrNumbers}, Users: {joinedDistinctFilteredPrUserNames}");
+            try
+            {
+                var prRequest = new PullRequestRequest { SortProperty = PullRequestSort.Created, SortDirection = SortDirection.Descending };
+                var prList = await _gitHubClient.Repository.PullRequest.GetAllForRepository(repository.Id, prRequest, apiOptions);
+                var prFilter = new Func<PullRequest, bool>(pr =>
+                    pr.CreatedAt > input.CollectionDate.AddDays(-collectionPeriodInDays) ||
+                    pr.UpdatedAt > input.CollectionDate.AddDays(-collectionPeriodInDays) ||
+                    pr.MergedAt > input.CollectionDate.AddDays(-collectionPeriodInDays) ||
+                    pr.ClosedAt > input.CollectionDate.AddDays(-collectionPeriodInDays));
+                var filteredPrList = prList.Where(prFilter);
+                var distinctFilteredPrUserNames = filteredPrList.Select(pr => pr.User.Login).Distinct();
+                var joinedDistinctFilteredPrUserNames = string.Join(',', distinctFilteredPrUserNames);
+                var filteredPrCountOverPeriod = filteredPrList.Count();
+                var filteredPrNumbers = string.Join(',', filteredPrList.Select(pr => pr.Number));
+                Console.WriteLine($"Repo: {repository.Name}, PRs: {filteredPrCountOverPeriod}, Numbers: {filteredPrNumbers}, Users: {joinedDistinctFilteredPrUserNames}");
 
-            return new PullRequestOutput(
-                filteredPrCountOverPeriod,
-                distinctFilteredPrUserNames,
-                joinedDistinctFilteredPrUserNames);
+                return new PullRequestOutput(
+                    filteredPrCountOverPeriod,
+                    distinctFilteredPrUserNames,
+                    joinedDistinctFilteredPrUserNames);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error getting pull request data for {Repository}: {Exception}", repository.Name, ex);
+                throw;
+            }
         }
     }
 
